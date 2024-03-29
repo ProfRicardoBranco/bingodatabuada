fde4bfd0eacd8b3234adef4aaeb28001e676eb1a
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-29 18:04:05 -0300

    modificando a tabela

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // const maximoBingo = 75; // Não é mais necessário
 // const numerosNaTabela = []; // Array para armazenar os números na tabela
-const arrayNumerosPossiveis = [6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 50, 54, 56, 60, 63, 64, 70, 72, 80, 81, 90];
+const arrayNumerosPossiveis = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 54, 56, 63, 64, 72, 81];
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     reiniciarJogo();
@@ -19,25 +19,27 @@ function reiniciarJogo() {
     // Limpa o conjunto de números sorteados
     numerosSorteados.clear();
 
-    // Preenche a tabela com os números possíveis
-    for (let i = 0; i < arrayNumerosPossiveis.length; i += 1) {
-        const row = bingoTable.insertRow();
-        for (let j = i; j < i + 1 && j < arrayNumerosPossiveis.length; j++) {
-            const cell = row.insertCell();
-            cell.textContent = arrayNumerosPossiveis[j];
-            cell.style.padding = '5px 10px';
-            cell.style.fontSize = '40px';
-            cell.style.fontWeight = 'bold';
+// Preenche a tabela com os números possíveis
+const numerosPorLinha = 6; // Quantidade de números por linha
+for (let i = 0; i < arrayNumerosPossiveis.length; i += numerosPorLinha) {
+    const row = bingoTable.insertRow();
+    for (let j = i; j < i + numerosPorLinha && j < arrayNumerosPossiveis.length; j++) {
+        const cell = row.insertCell();
+        cell.textContent = arrayNumerosPossiveis[j];
+        cell.style.padding = '5px 10px';
+        cell.style.fontSize = '40px';
+        cell.style.fontWeight = 'bold';
 
-            if ((j + 1) % 2 === 0) {
-                // Se o número for par, define a cor de fundo como branco
-                cell.style.backgroundColor = '#ffffff';
-            } else {
-                // Se o número for ímpar, define a cor de fundo como cinza claro
-                cell.style.backgroundColor = '#f2f2f2';
-            }
+        if ((j + 1) % 2 === 0) {
+            // Se o número for par, define a cor de fundo como branco
+            cell.style.backgroundColor = '#ffffff';
+        } else {
+            // Se o número for ímpar, define a cor de fundo como cinza claro
+            cell.style.backgroundColor = '#f2f2f2';
         }
     }
+}
+
 }
 
 // Função para gerar um número aleatório
