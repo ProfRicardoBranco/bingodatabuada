e09783175c293ec3dcb4d93621ba6e953faf8925
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-29 22:25:04 -0300

    asdasd

--- desafio.js
+++ desafio.js
@@ -1,20 +1,31 @@
 // Array com os números possíveis
-const arrayNumerosPossiveis = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 54, 56, 63, 64, 72, 81, 50, 60, 70, 80, 90];
+const arrayNumerosPossiveis = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 54, 56, 63, 64, 72, 81,50,60,70,80,90];
 
 // Elementos da interface
+const bingoTable = document.getElementById('bingoTable');
 const sortearBtn = document.getElementById('sortearBtn');
 const novoJogoBtn = document.getElementById('novoJogoBtn');
-const ultimaMultiplicacao = document.getElementById('ultimaMultiplicacao');
-const tabelaNumerosSorteados = document.getElementById('tabelaNumerosSorteados');
-
-// Conjunto para armazenar números sorteados
-const numerosSorteados = new Set();
+const btnMarcarResultado = document.getElementById('btnMarcarResultado');
+const numeroSorteadoText = document.getElementById('numeroSorteado');
+const numerosSorteados = new Set(); // Conjunto para armazenar números sorteados
+let ultimoIndiceSorteado; // Variável global para armazenar o último índice sorteado
+let sorteioHabilitado = true; // Variável global para controlar se o sorteio está habilitado
 
 // Evento de clique no botão "Novo Jogo"
-novoJogoBtn.addEventListener('click', reiniciarJogo);
+novoJogoBtn.addEventListener('click', () => {
+    reiniciarJogo();
+    numeroSorteadoText.innerHTML = ""; // Limpa o conteúdo do elemento
+    sortearBtn.disabled = false; // Habilita o botão de sortear
+    sortearBtn.style.backgroundColor = ''; // Remove a cor de fundo personalizada
+    sorteioHabilitado = true; // Habilita o sorteio
+});
 
 // Evento de clique no botão "Sortear"
 sortearBtn.addEventListener('click', () => {
+    if (!sorteioHabilitado) {
+        return; // Interrompe a execução se o sorteio não estiver habilitado
+    }
+
     // Verifica se todos os números já foram sorteados
     if (numerosSorteados.size === arrayNumerosPossiveis.length) {
         alert('Todos os números já foram sorteados!');
@@ -27,17 +38,64 @@ sortearBtn.addEventListener('click', () => {
         indiceSorteado = gerarNumeroAleatorio();
     } while (numerosSorteados.has(indiceSorteado));
 
+
+    if (ultimoIndiceSorteado !== undefined) {
+        marcarNumeroSorteado(ultimoIndiceSorteado);
+    }
+
     numerosSorteados.add(indiceSorteado);
+    ultimoIndiceSorteado = indiceSorteado;
     atualizarNumeroSorteado(arrayNumerosPossiveis[indiceSorteado]);
+
+    // Desabilita o botão de sortear após o sorteio
+    sortearBtn.disabled = true;
+    sortearBtn.style.backgroundColor = '#6c757d'; // Cor cinza azulado
+    sortearBtn.style.color = '#E3E5E7'
+    sorteioHabilitado = false; // Desabilita o sorteio
+});
+
+// Evento de clique no botão "Marcar Resultado"
+btnMarcarResultado.addEventListener('click', () => {
+    // Muda a variável sorteioHabilitado para true
+    sorteioHabilitado = true;
+    sortearBtn.disabled = false; // Habilita o botão de sortear
+    sortearBtn.style.backgroundColor = ''; // Remove a cor de fundo personalizada
+    sortearBtn.style.color = ''; // Remove a cor de fundo personalizada
+    if (ultimoIndiceSorteado !== undefined) {
+        marcarNumeroSorteado(ultimoIndiceSorteado);
+    } else {
+        alert('Nenhum número foi sorteado ainda!');
+    }
 });
 
 // Função para reiniciar o jogo
 function reiniciarJogo() {
     // Limpa a tabela
-    tabelaNumerosSorteados.innerHTML = '';
+    bingoTable.innerHTML = '';
 
     // Limpa o conjunto de números sorteados
     numerosSorteados.clear();
+
+    // Preenche a tabela com os números possíveis
+    const numerosPorLinha = 6; // Quantidade de números por linha
+    for (let i = 0; i < arrayNumerosPossiveis.length; i += numerosPorLinha) {
+        const row = bingoTable.insertRow();
+        for (let j = i; j < i + numerosPorLinha && j < arrayNumerosPossiveis.length; j++) {
+            const cell = row.insertCell();
+            cell.textContent = arrayNumerosPossiveis[j];
+            cell.style.padding = '5px 10px';
+            cell.style.fontSize = '40px';
+            cell.style.fontWeight = 'bold';
+
+            if ((j + 1) % 2 === 0) {
+                // Se o número for par, define a cor de fundo como branco
+                cell.style.backgroundColor = '#ffffff';
+            } else {
+                // Se o número for ímpar, define a cor de fundo como cinza claro
+                cell.style.backgroundColor = '#f2f2f2';
+            }
+        }
+    }
 }
 
 // Função para gerar um número aleatório
@@ -58,14 +116,41 @@ function atualizarNumeroSorteado(numero) {
         }
     }
 
+    // Se o primeiro fator for 10, inverte os fatores
+    if (fator1 === 10) {
+        const temp = fator1;
+        fator1 = fator2;
+        fator2 = temp;
+    }
+
     // Exibe a multiplicação dos dois fatores na tela
     if (fator1 && fator2) {
-        const tr = tabelaNumerosSorteados.insertRow();
-        const cell1 = tr.insertCell();
-        const cell2 = tr.insertCell();
-        cell1.textContent = `${fator1} * ${fator2}`;
-        cell2.textContent = numero;
+        numeroSorteadoText.innerHTML = "<span style='font-size: 30px;'>Dê o resultado de: " + "<br/>" + 
+                                        "<span style='font-size: 70px; font-weight: bold; color: green;'>" + 
+                                        fator1 + " X " + fator2 +  
+                                        "</span>";
     } else {
-        ultimaMultiplicacao.textContent = `Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número ${numero}.`;
+        numeroSorteadoText.innerHTML = "<span style='font-size: 30px;'>Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número " + numero + ".</span>";
     }
 }
+
+
+
+// Função para marcar o número sorteado na tabela
+function marcarNumeroSorteado(indice) {
+    const numeroSorteado = arrayNumerosPossiveis[indice];
+    console.log('Marcando número sorteado:', numeroSorteado);
+    const cells = bingoTable.querySelectorAll('td');
+    for (const cell of cells) {
+        if (parseInt(cell.textContent) === numeroSorteado) {
+            cell.classList.add('marked');
+            cell.style.backgroundColor = '#28a745'; // Altera a cor de fundo da célula
+            break;
+        }
+    }
+}
+
+// Função para inicializar o jogo quando a página é carregada
+window.addEventListener('DOMContentLoaded', () => {
+    reiniciarJogo();
+});
--- script.js
+++ script.js
@@ -5,14 +5,13 @@ const arrayNumerosPossiveis = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 2
 const sortearBtn = document.getElementById('sortearBtn');
 const novoJogoBtn = document.getElementById('novoJogoBtn');
 const ultimaMultiplicacao = document.getElementById('ultimaMultiplicacao');
-const numerosSorteados = new Set(); // Conjunto para armazenar números sorteados
-let linhaAtual = 0; // Variável global para controlar a linha atual na tabela de multiplicação
+const tabelaNumerosSorteados = document.getElementById('tabelaNumerosSorteados');
+
+// Conjunto para armazenar números sorteados
+const numerosSorteados = new Set();
 
 // Evento de clique no botão "Novo Jogo"
-novoJogoBtn.addEventListener('click', () => {
-    reiniciarJogo();
-    ultimaMultiplicacao.innerHTML = ""; // Limpa o conteúdo do elemento
-});
+novoJogoBtn.addEventListener('click', reiniciarJogo);
 
 // Evento de clique no botão "Sortear"
 sortearBtn.addEventListener('click', () => {
@@ -34,18 +33,11 @@ sortearBtn.addEventListener('click', () => {
 
 // Função para reiniciar o jogo
 function reiniciarJogo() {
+    // Limpa a tabela
+    tabelaNumerosSorteados.innerHTML = '';
+
     // Limpa o conjunto de números sorteados
     numerosSorteados.clear();
-
-    // Reinicia a linha atual
-    linhaAtual = 0;
-
-    // Limpa as células da tabela
-    const tabelaNumerosSorteados = document.getElementById('tabelaNumerosSorteados');
-    const cells = tabelaNumerosSorteados.querySelectorAll('td');
-    cells.forEach(cell => {
-        cell.textContent = '';
-    });
 }
 
 // Função para gerar um número aleatório
@@ -53,7 +45,7 @@ function gerarNumeroAleatorio() {
     return Math.floor(Math.random() * arrayNumerosPossiveis.length);
 }
 
-// Função para atualizar o texto com o número sorteado na tela e preencher a tabela
+// Função para atualizar o texto com o número sorteado na tela
 function atualizarNumeroSorteado(numero) {
     let fator1, fator2;
 
@@ -68,26 +60,12 @@ function atualizarNumeroSorteado(numero) {
 
     // Exibe a multiplicação dos dois fatores na tela
     if (fator1 && fator2) {
-        ultimaMultiplicacao.innerHTML = "<span style='font-size: 30px;'>O resultado de: " +  "</span><br/>" + 
-                                        "<span style='font-size: 70px; font-weight: bold; color: green;'>" + 
-                                        fator1 + " * " + fator2 + 
-                                        "</span>";
-                                        
-        // Preenche a tabela com a multiplicação
-        const tabelaNumerosSorteados = document.getElementById('tabelaNumerosSorteados');
-        const linha = tabelaNumerosSorteados.rows[linhaAtual];
-        linha.cells[0].textContent = fator1;
-        linha.cells[1].textContent = '*';
-        linha.cells[2].textContent = fator2;
-        linha.cells[3].textContent = '=';
-        linha.cells[4].textContent = numero;
-        linhaAtual++; // Incrementa a linha atual
+        const tr = tabelaNumerosSorteados.insertRow();
+        const cell1 = tr.insertCell();
+        const cell2 = tr.insertCell();
+        cell1.textContent = `${fator1} * ${fator2}`;
+        cell2.textContent = numero;
     } else {
-        ultimaMultiplicacao.innerHTML = "<span style='font-size: 30px;'>Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número " + numero + ".</span>";
+        ultimaMultiplicacao.textContent = `Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número ${numero}.`;
     }
 }
-
-// Função para inicializar o jogo quando a página é carregada
-window.addEventListener('DOMContentLoaded', () => {
-    reiniciarJogo();
-});
