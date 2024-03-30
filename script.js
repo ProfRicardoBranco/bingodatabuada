82644cab1f2ffb94e8bf79adbfb7e461d60c04fc
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-29 22:07:32 -0300

    dasdas

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 const arrayNumerosPossiveis = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 54, 56, 63, 64, 72, 81,50,60,70,80,90];
 
 // Elementos da interface
-const bingoTable = document.getElementById('bingoTable');
 const sortearBtn = document.getElementById('sortearBtn');
 const novoJogoBtn = document.getElementById('novoJogoBtn');
-const numeroSorteadoText = document.getElementById('numeroSorteado');
+const ultimaMultiplicacao = document.getElementById('ultimaMultiplicacao');
+const logMultiplicacoes = document.getElementById('logMultiplicacoes');
 const numerosSorteados = new Set(); // Conjunto para armazenar números sorteados
 
 // Evento de clique no botão "Novo Jogo"
 novoJogoBtn.addEventListener('click', () => {
     reiniciarJogo();
     ultimaMultiplicacao.innerHTML = ""; // Limpa o conteúdo do elemento
+    logMultiplicacoes.innerHTML = ""; // Limpa o log de multiplicacoes
 });
 
 // Evento de clique no botão "Sortear"
@@ -30,15 +31,11 @@ sortearBtn.addEventListener('click', () => {
 
     numerosSorteados.add(indiceSorteado);
     atualizarNumeroSorteado(arrayNumerosPossiveis[indiceSorteado]);
-
+    registrarMultiplicacao(arrayNumerosPossiveis[indiceSorteado]);
 });
 
 // Função para reiniciar o jogo
 function reiniciarJogo() {
-    // Limpa a tabela
-    //bingoTable.innerHTML = '';
-
-    // Limpa o conjunto de números sorteados
     numerosSorteados.clear();
 }
 
@@ -71,9 +68,23 @@ function atualizarNumeroSorteado(numero) {
     }
 }
 
-// Função para marcar o número sorteado na tabela
-function marcarNumeroSorteado(indice) {
-    // Esta função não será utilizada na nova versão, pois não há mais tabela
+// Função para registrar a multiplicação sorteada no log
+function registrarMultiplicacao(numero) {
+    let fator1, fator2;
+    for (let i = 2; i <= 10; i++) {
+        if (numero % i === 0 && numero / i <= 9) {
+            fator1 = i;
+            fator2 = numero / i;
+            break;
+        }
+    }
+
+    if (fator1 && fator2) {
+        const multiplicacao = fator1 + " * " + fator2;
+        const multiplicacaoItem = document.createElement('p');
+        multiplicacaoItem.textContent = multiplicacao;
+        logMultiplicacoes.appendChild(multiplicacaoItem);
+    }
 }
 
 // Função para inicializar o jogo quando a página é carregada
