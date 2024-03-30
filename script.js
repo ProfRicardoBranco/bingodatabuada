089dee1c3ff019edf7e4e4417a8783cb8ce6db44
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-29 21:56:21 -0300

    asdasd

--- script.js
+++ script.js
@@ -4,27 +4,17 @@ const arrayNumerosPossiveis = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 2
 // Elementos da interface
 const novoJogoBtn = document.getElementById('novoJogoBtn');
 const sortearBtn = document.getElementById('sortearBtn');
-const btnMarcarResultado = document.getElementById('btnMarcarResultado');
-const numeroSorteadoText = document.getElementById('numeroSorteado');
 const logMultiplicacoes = document.getElementById('logMultiplicacoes');
 const ultimaMultiplicacaoText = document.getElementById('ultimaMultiplicacao');
-
-let sorteioHabilitado = true; // Variável global para controlar se o sorteio está habilitado
 
 // Evento de clique no botão "Novo Jogo"
 novoJogoBtn.addEventListener('click', () => {
     reiniciarJogo();
     numeroSorteadoText.innerHTML = ""; // Limpa o conteúdo do elemento
-    sorteioHabilitado = true; // Habilita o sorteio
-    atualizarCorBotaoSortear(); // Atualiza a cor do botão "Sortear"
 });
 
 // Evento de clique no botão "Sortear"
 sortearBtn.addEventListener('click', () => {
-    if (!sorteioHabilitado) {
-        return; // Interrompe a execução se o sorteio não estiver habilitado
-    }
-
     // Verifica se todos os números já foram sorteados
     if (logMultiplicacoes.childElementCount === arrayNumerosPossiveis.length) {
         alert('Todos os números já foram sorteados!');
@@ -62,17 +52,6 @@ sortearBtn.addEventListener('click', () => {
     } else {
         console.error(`Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número ${numeroSorteado}.`);
     }
-
-    // Desabilita o botão de sortear após o sorteio
-    sorteioHabilitado = false;
-    atualizarCorBotaoSortear(); // Atualiza a cor do botão "Sortear"
-});
-
-// Evento de clique no botão "Marcar Resultado"
-btnMarcarResultado.addEventListener('click', () => {
-    // Muda a variável sorteioHabilitado para true
-    sorteioHabilitado = true;
-    atualizarCorBotaoSortear(); // Atualiza a cor do botão "Sortear"
 });
 
 // Função para reiniciar o jogo
@@ -85,15 +64,4 @@ function reiniciarJogo() {
 // Função para gerar um número aleatório
 function gerarNumeroAleatorio() {
     return Math.floor(Math.random() * arrayNumerosPossiveis.length);
-}
-
-// Função para atualizar a cor do botão "Sortear" com base no estado do sorteio
-function atualizarCorBotaoSortear() {
-    if (sorteioHabilitado) {
-        sortearBtn.disabled = false;
-        sortearBtn.style.backgroundColor = ''; // Remove a cor de fundo personalizada
-    } else {
-        sortearBtn.disabled = true;
-        sortearBtn.style.backgroundColor = '#6c757d'; // Cor cinza azulado
-    }
 }
