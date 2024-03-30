9602f39b61c3a0d129f817644d4f612b7545a8d0
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-29 21:49:17 -0300

    das

--- script.js
+++ script.js
@@ -2,11 +2,21 @@
 const arrayNumerosPossiveis = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 54, 56, 63, 64, 72, 81, 50, 60, 70, 80, 90];
 
 // Elementos da interface
+const novoJogoBtn = document.getElementById('novoJogoBtn');
 const sortearBtn = document.getElementById('sortearBtn');
+const btnMarcarResultado = document.getElementById('btnMarcarResultado');
 const numeroSorteadoText = document.getElementById('numeroSorteado');
-const numerosSorteados = new Set(); // Conjunto para armazenar números sorteados
-let ultimoIndiceSorteado; // Variável global para armazenar o último índice sorteado
+const logMultiplicacoes = document.getElementById('logMultiplicacoes');
+
 let sorteioHabilitado = true; // Variável global para controlar se o sorteio está habilitado
+
+// Evento de clique no botão "Novo Jogo"
+novoJogoBtn.addEventListener('click', () => {
+    reiniciarJogo();
+    numeroSorteadoText.innerHTML = ""; // Limpa o conteúdo do elemento
+    sorteioHabilitado = true; // Habilita o sorteio
+    atualizarCorBotaoSortear(); // Atualiza a cor do botão "Sortear"
+});
 
 // Evento de clique no botão "Sortear"
 sortearBtn.addEventListener('click', () => {
@@ -15,7 +25,7 @@ sortearBtn.addEventListener('click', () => {
     }
 
     // Verifica se todos os números já foram sorteados
-    if (numerosSorteados.size === arrayNumerosPossiveis.length) {
+    if (logMultiplicacoes.childElementCount === arrayNumerosPossiveis.length) {
         alert('Todos os números já foram sorteados!');
         return;
     }
@@ -24,50 +34,9 @@ sortearBtn.addEventListener('click', () => {
 
     do {
         indiceSorteado = gerarNumeroAleatorio();
-    } while (numerosSorteados.has(indiceSorteado));
+    } while (logMultiplicacoes.querySelector(`#multiplicacao-${indiceSorteado}`));
 
-
-    if (ultimoIndiceSorteado !== undefined) {
-        registrarMultiplicacao(ultimoIndiceSorteado);
-    }
-
-    numerosSorteados.add(indiceSorteado);
-    ultimoIndiceSorteado = indiceSorteado;
-    atualizarNumeroSorteado(arrayNumerosPossiveis[indiceSorteado]);
-
-    // Desabilita o botão de sortear após o sorteio
-    sorteioHabilitado = false; // Desabilita o sorteio
-});
-
-// Função para gerar um número aleatório
-function gerarNumeroAleatorio() {
-    return Math.floor(Math.random() * arrayNumerosPossiveis.length);
-}
-
-// Função para atualizar o texto com o número sorteado na tela
-function atualizarNumeroSorteado(numero) {
-    let fator1, fator2;
-
-    // Procura por um fator que seja da tabuada de 2 a 9 e menor que 10
-    for (let i = 2; i <= 10; i++) {
-        if (numero % i === 0 && numero / i <= 9) {
-            fator1 = i;
-            fator2 = numero / i;
-            break;
-        }
-    }
-
-    // Exibe a multiplicação dos dois fatores na tela
-    if (fator1 && fator2) {
-        numeroSorteadoText.textContent = "Multiplicação Sorteada: " + fator1 + " * " + fator2;
-    } else {
-        numeroSorteadoText.textContent = "Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número " + numero + ".";
-    }
-}
-
-// Função para registrar a multiplicação sorteada
-function registrarMultiplicacao(indice) {
-    const numeroSorteado = arrayNumerosPossiveis[indice];
+    const numeroSorteado = arrayNumerosPossiveis[indiceSorteado];
     let fator1, fator2;
 
     // Procura por um fator que seja da tabuada de 2 a 9 e menor que 10
@@ -79,14 +48,47 @@ function registrarMultiplicacao(indice) {
         }
     }
 
-    // Exibe a multiplicação dos dois fatores no log
+    // Exibe a multiplicação dos dois fatores na tela
     if (fator1 && fator2) {
-        const logMultiplicacao = document.createElement('p');
-        logMultiplicacao.textContent = "Multiplicação Sorteada: " + fator1 + " * " + fator2;
-        document.getElementById('logMultiplicacoes').appendChild(logMultiplicacao);
+        const multiplicacao = `${fator1} * ${fator2}`;
+        const logItem = document.createElement('p');
+        logItem.textContent = multiplicacao;
+        logItem.id = `multiplicacao-${indiceSorteado}`;
+        logMultiplicacoes.appendChild(logItem);
     } else {
-        const logErro = document.createElement('p');
-        logErro.textContent = "Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número " + numeroSorteado + ".";
-        document.getElementById('logMultiplicacoes').appendChild(logErro);
+        console.error(`Não foi possível encontrar fatores menores que 10 da tabuada de 2 a 9 para o número ${numeroSorteado}.`);
+    }
+
+    // Desabilita o botão de sortear após o sorteio
+    sorteioHabilitado = false;
+    atualizarCorBotaoSortear(); // Atualiza a cor do botão "Sortear"
+});
+
+// Evento de clique no botão "Marcar Resultado"
+btnMarcarResultado.addEventListener('click', () => {
+    // Muda a variável sorteioHabilitado para true
+    sorteioHabilitado = true;
+    atualizarCorBotaoSortear(); // Atualiza a cor do botão "Sortear"
+});
+
+// Função para reiniciar o jogo
+function reiniciarJogo() {
+    // Limpa o log de multiplicacoes
+    logMultiplicacoes.innerHTML = '';
+}
+
+// Função para gerar um número aleatório
+function gerarNumeroAleatorio() {
+    return Math.floor(Math.random() * arrayNumerosPossiveis.length);
+}
+
+// Função para atualizar a cor do botão "Sortear" com base no estado do sorteio
+function atualizarCorBotaoSortear() {
+    if (sorteioHabilitado) {
+        sortearBtn.disabled = false;
+        sortearBtn.style.backgroundColor = ''; // Remove a cor de fundo personalizada
+    } else {
+        sortearBtn.disabled = true;
+        sortearBtn.style.backgroundColor = '#6c757d'; // Cor cinza azulado
     }
 }
